e4f34c7b16f7f687b7d4bc89b2a219bf57c80139
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-01-31 16:15:14 +0000

    Update calculateDivisors.js
    task 4

--- task-4-calculateDivisors/calculateDivisors.js
+++ task-4-calculateDivisors/calculateDivisors.js
@@ -1,23 +1,30 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
 function calculateDivisors(number) {
-	const divisors = [];
-	for (let i = 1; i <= number; i++) {
-		if (number % i === 0) {
-			divisors.push(i);
-		}
-	}
-	return divisors;
-}
+   let sum = 0;
 
-runTest(
-	"should return all divisors of a given number",
-	function () {
-		// ...add check() assertions here
-		check(calculateDivisors(1)).isEqualTo([1]);
-		check(calculateDivisors(6)).isEqualTo([1, 2, 3, 6]);
-	}
-);
-runTest("should return all divisors of a given Prime number", function () {
-	check(calculateDivisors(17)).isEqualTo([1, 17]);
+    for (let i = 1; i < number; i++) {
+        if (i % 3 === 0 || i % 5 === 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+runTest("should return 0 for input 1", function () {
+    check(calculateDivisors(1), 0);
 });
+runTest("should return 8 for input 6", function () {
+    check(calculateDivisors(6), 8);
+});
+
+runTest("should return 23 for input 10", function () {
+    check(calculateDivisors(10), 23);
+});
+
+runTest("should return 33 for input 12", function () {
+    check(calculateDivisors(12), 33);
+});
+
+
+
