0439c326169139e59007a96817fbef3a391ad90b
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-01-31 15:44:02 +0000

    task 5

--- task-5-sentenceToCamelCase/sentenceToCamelCase.js
+++ task-5-sentenceToCamelCase/sentenceToCamelCase.js
@@ -1,10 +1,19 @@
 const { check, runTest, skipTest } = require("../test-api/index.js");
 
-function sentenceToCamelCase() {}
+function sentenceToCamelCase(sentence) {
+	let words = sentence.toLowerCase().split(" ");
+	let camelCaseStr = words[0] || "";
 
-runTest(
-	"change this to describe the behaviour you are testing in this block",
-	function () {
-		// ...add check() assertions here
+	for (let i = 1; i < words.length; i++) {
+		if (words[i]) {
+			camelCaseStr += words[i][0].toUpperCase() + words[i].slice(1);
+		}
 	}
-);
+
+	return camelCaseStr;
+}
+
+runTest("should convert a sentence to camelCase", function () {
+	check(sentenceToCamelCase("hello world")).isEqualTo("helloWorld");
+
+});
